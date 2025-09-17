edd4eb46473ace81994d602cd8ed96063dfcacc0
Chiru DEy <chiru.d.guy@gmail.com>
2025-09-17 11:45:51 +0530

    ui and icon

--- static/js/collapsible.js
+++ static/js/collapsible.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     var coll = document.getElementsByClassName("collapsible");
     for (var i = 0; i < coll.length; i++) {
+        // Expand active sections on page load
+        if (coll[i].classList.contains("active")) {
+            var content = coll[i].nextElementSibling;
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+
         coll[i].addEventListener("click", function() {
             this.classList.toggle("active");
             var content = this.nextElementSibling;
@@ -8,7 +14,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 content.style.maxHeight = null;
             } else {
                 content.style.maxHeight = content.scrollHeight + "px";
-            } 
+            }
         });
     }
 });
